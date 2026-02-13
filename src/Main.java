73770718d4640eba13c39ce871a83cd47a4c123a
irene66787 <akiraphonk@gmail.com>
2026-02-13 09:23:46 +0400

    2

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ public class Main {
         BankAccount accountSecond = new BankAccount("user2");
 
         accountFirst.deposit(1000);
-        System.out.println("balance " + accountFirst.checkBalance());
+        System.out.println("balance: " + accountFirst.checkBalance());
 
         accountFirst.withdraw(500);
-        System.out.println("balance " + accountFirst.checkBalance());
+        System.out.println("balance: " + accountFirst.checkBalance());
 
         boolean res = accountFirst.transfer(accountSecond, 200);
         System.out.println("transfer: " + res);
 
-        System.out.println("user1" + ": " + accountFirst.checkBalance());
-        System.out.println("user2" + ": " + accountSecond.checkBalance());
+        System.out.println("user1 :" + accountFirst.checkBalance());
+        System.out.println("user2 :" + accountSecond.checkBalance());
     }
 }
