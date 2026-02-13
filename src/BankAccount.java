d0813f2fad5e52d88348ae47028df39313fed7d2
irene66787 <akiraphonk@gmail.com>
2026-02-13 09:22:14 +0400

    1

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -12,4 +12,39 @@ public class BankAccount {
         this.openDate = LocalDateTime.now();
         this.isBlocked = false;
     }
+
+    public int checkBalance() {
+        return balance;
+    }
+
+    public boolean deposit(int amount) {
+        if(!isBlocked && amount > 0) {
+            balance += amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean withdraw (int amount) {
+        if(!isBlocked && amount > 0 && amount <= balance) {
+            balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transfer(BankAccount otherAccount, int amount) {
+        if (!this.withdraw(amount)) {
+            return false;
+        }
+
+        if (otherAccount.deposit(amount)) {
+            return true;
+        }
+
+        this.balance += amount;
+        return false;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        BankAccount accountFirst = new BankAccount("user1");
+        BankAccount accountSecond = new BankAccount("user2");
+
+        accountFirst.deposit(1000);
+        System.out.println("balance " + accountFirst.checkBalance());
+
+        accountFirst.withdraw(500);
+        System.out.println("balance " + accountFirst.checkBalance());
+
+        boolean res = accountFirst.transfer(accountSecond, 200);
+        System.out.println("transfer: " + res);
+
+        System.out.println("user1" + ": " + accountFirst.checkBalance());
+        System.out.println("user2" + ": " + accountSecond.checkBalance());
+    }
 }
